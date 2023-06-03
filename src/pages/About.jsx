6feb520da08a89d7d268a1d6a27c79bbdecf1d16
SergeySidorov <sidorovserega92@gmail.com>
2023-06-03 17:46:54 +0300

    add page About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Card } from 'react-bootstrap';
+import avatarUser from '../assets/img/avatar-user.svg';
 
 const About = () => {
   return (
-    <div>About</div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={avatarUser} />
+      <Card.Body>
+        <Card.Title>Сидоров Сергей</Card.Title>
+        <Card.Text>
+          Здравствуйте! Я начинающий Frontend-разработчик. 
+          Проработав долгое время на государственной службе, осознал, что мое развитие остановилось, 
+          принял решение сменить профессию. Выбрал направление Frontend-разработки из-за личной симпатии к данному направлению, 
+          возможности постоянного роста и развития своих профессиональных навыков, карьерного роста, отсутствия бюрократической волокиты.
+          В процессе обучения сделал несколько учебных проектов, с которыми можно ознакомиться...
+        </Card.Text>
+      </Card.Body>
+    </Card>
   )
 }
 
